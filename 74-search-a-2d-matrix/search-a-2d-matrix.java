9c775612780fda88c14a00fd6c4051be0ec868b1
Priyanshu kumar <70458921+priyanshu1947@users.noreply.github.com>
2024-02-27 23:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (40.83%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,41 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
+        int m = matrix[0].length;
         if (n == 0) {
             return false;  // Matrix is empty, target cannot be found
         }
-
-        int m = matrix[0].length;
-
-        // Perform a binary search on each row
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
-                // If the target may exist in this row, perform binary search on this row
-                if (binarySearch(matrix[i], target)) {
-                    return true;  // Target found
+        for(int i =0; i<n;i++)
+        {
+            if(matrix[i][0]<=target && target <=matrix[i][m-1])
+            {
+                if(binarySearch(matrix[i], target))
+                {
+                    return true;
                 }
             }
-        }
 
-        return false;  // Target not found in the matrix
+        }
+        return false;
     }
 
-    private boolean binarySearch(int[] row, int target) {
-        int left = 0;
-        int right = row.length - 1;
+    public boolean binarySearch( int [] row, int target)
+    {
+        int left =0;
+        int right = row.length-1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (row[mid] == target) {
-                return true;  // Target found
-            } else if (row[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        while(left<=right)
+        {
+            int mid = left+(right-left)/2;
+            if(row[mid]== target)
+            {
+                return true;
+            }
+            else if(row[mid]<target)
+            {
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
             }
         }
-
-        return false;  // Target not found in this row
+        return false;
     }
 }

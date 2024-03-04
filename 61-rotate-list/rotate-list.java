b10c75121ca2b3ea4ae7180cad7594d35484d3f2
Priyanshu kumar <70458921+priyanshu1947@users.noreply.github.com>
2024-03-04 21:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (52.48%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null)
+            return null;
+
+        ListNode curr = head;
+        int size = 1;
+
+        // Calculate the length of the linked list and make it circular
+        while (curr.next != null) {
+            curr = curr.next;
+            ++size;
+        }
+
+        curr.next = head;  // Make the linked list circular
+
+        // Cut the linked list from the rotating point
+        int i = size - (k % size);
+
+        while (i > 1) {
+            head = head.next;
+            i--;
+        }
+
+        curr = head.next;  // New head of the rotated linked list
+        head.next = null;  // Cut the linked list at the rotating point
+        return curr;
+    }
+}
